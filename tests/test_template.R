88e5dc8cf838322675899fa6342da857b6e74884
Zheng <chaozhi.zheng@wur.nl>
2020-05-14 10:06:06 +0200

    delete test

--- tests/test_template.R
+++
@@ -1,8 +0,0 @@
-
-try1 <- try(1==1,silent = TRUE)
-
-test_that("no error in testing ...", {
-  
-  expect_false(inherits(try1, "try-error"))
-  
-})
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(PolyOriginR)
-
-test_check("PolyOriginR")
